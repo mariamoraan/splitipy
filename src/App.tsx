d650569791212a5108bce705becbc4d187e2982d
mariamoraan <mariamoranluaces@gmail.com>
2024-01-01 20:31:19 +0100

    Project set up

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
+
+
+
+const App = () => {   
+    const router = createBrowserRouter([
+       
+    ]);
+
+   return (
+    <RouterProvider router={router} />
+   )
+};
+
+export default App;
